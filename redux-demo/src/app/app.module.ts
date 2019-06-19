b7deccae0ac5cb714dece52983518d4a5da55d77
soumyadip007 <soumyadip.note@gmail.com>
2019-06-19 12:04:06 +0530

    devTools Added

--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgReduxModule, NgRedux } from '@angular-redux/store';
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
 import { AppComponent } from './app.component';
 import { IAppState , rootReducer, INITIAL_STATE} from './store';
 
@@ -17,7 +17,12 @@ import { IAppState , rootReducer, INITIAL_STATE} from './store';
   bootstrap: [AppComponent]
 })
   export class AppModule {
-    constructor(ngRedux: NgRedux<IAppState>) {
-        ngRedux.configureStore(rootReducer, INITIAL_STATE);
+    
+    constructor(
+      ngRedux: NgRedux<IAppState>,
+      devTools : DevToolsExtension) {
+
+        var enhancer=isDevMode()? [devTools.enhancer()] : [];
+        ngRedux.configureStore(rootReducer, INITIAL_STATE,[], enhancer);
     }
 }
